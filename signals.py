ce095387da5616630f617e0b178bfd30fffc183f
Nezih72 <81173110+Nezih72@users.noreply.github.com>
2026-07-27 14:41:29 +0300

    Add signal generation function based on RSI and MACD

--- signals.py
+++ signals.py
@@ -1,1 +1,10 @@
+def generate_signal(rsi, macd, macd_signal):
 
+    if rsi < 35 and macd > macd_signal:
+        return "🟢 AL"
+
+    elif rsi > 70 and macd < macd_signal:
+        return "🔴 SAT"
+
+    else:
+        return "🟡 BEKLE"
