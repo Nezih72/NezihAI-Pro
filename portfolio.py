0a5b3e54fa553417b7c97bc0becaa440544b6890
Nezih72 <81173110+Nezih72@users.noreply.github.com>
2026-07-27 14:48:48 +0300

    Add Portfolio class to manage coin positions

--- portfolio.py
+++ portfolio.py
@@ -1,1 +1,25 @@
+class Portfolio:
 
+    def __init__(self):
+
+        self.balance = 0
+
+        self.positions = []
+
+    def add_position(self, coin, amount, buy_price):
+
+        self.positions.append({
+            "coin": coin,
+            "amount": amount,
+            "buy_price": buy_price
+        })
+
+    def show(self):
+
+        for p in self.positions:
+
+            print(
+                f"{p['coin']} | "
+                f"{p['amount']} adet | "
+                f"Alış: {p['buy_price']}"
+            )
